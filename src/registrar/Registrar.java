3797307eb163038c7b25017f1982fbb9d0e6a965
Revollito <Revollito@Revollo>
2018-09-27 20:17:31 -0300

    mi aporte iu para registrar

--- src/registrar/Registrar.java
+++ src/registrar/Registrar.java
@@ -15,7 +15,8 @@ public class Registrar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       new Registro().setVisible(true);
+       
     }
     
 }
