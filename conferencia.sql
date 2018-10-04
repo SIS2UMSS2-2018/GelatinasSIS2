2cd58cd8e1556704ceeef9222ba91d779e81a817
sabo2525 <cbrt113@gmail.com>
2018-10-03 21:19:29 -0400

    baseDeDatosFinal :v

--- conferencia.sql
+++ conferencia.sql
@@ -1,20 +1,20 @@
 CREATE TABLE asistentes (
-  id_asistente INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  ci_asis INTEGER UNSIGNED NOT NULL,
   nombre_asis VARCHAR(15) NOT NULL,
-  ci_asis VARCHAR(15) NOT NULL,
-  ocupacion_asis VARCHAR(20) NOT NULL,
   apellido_asis VARCHAR(30) NOT NULL,
+  ocupacion_asis VARCHAR(20) NOT NULL,
   correo_asis VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id_asistente)
+  PRIMARY KEY(ci_asis)
 );
 
-CREATE TABLE asisten_grupo (
-  asistentes_id_asistente INTEGER UNSIGNED NOT NULL,
+CREATE TABLE cronograma (
+  id_crono INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   grupos_id_grupo INTEGER UNSIGNED NOT NULL,
-  asistido CHAR(1) NOT NULL,
-  PRIMARY KEY(asistentes_id_asistente, grupos_id_grupo),
-  INDEX asisten_grupo_FKIndex1(asistentes_id_asistente),
-  INDEX asisten_grupo_FKIndex2(grupos_id_grupo)
+  expositores_id_expo INTEGER UNSIGNED NOT NULL,
+  hora TIME NOT NULL,
+  PRIMARY KEY(id_crono),
+  INDEX cronograma_FKIndex1(expositores_id_expo),
+  INDEX cronograma_FKIndex2(grupos_id_grupo)
 );
 
 CREATE TABLE expositores (
@@ -28,28 +28,26 @@ CREATE TABLE expositores (
 
 CREATE TABLE grupos (
   id_grupo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  horas_id_hora INTEGER UNSIGNED NOT NULL,
-  temas_id_tema INTEGER UNSIGNED NOT NULL,
-  expositores_id_expo INTEGER UNSIGNED NOT NULL,
   cupos_dispo INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY(id_grupo),
-  INDEX grupos_FKIndex1(expositores_id_expo),
-  INDEX grupos_FKIndex2(temas_id_tema),
-  INDEX grupos_FKIndex3(horas_id_hora)
+  PRIMARY KEY(id_grupo)
 );
 
-CREATE TABLE horas (
-  id_hora INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  hora_in TIME NOT NULL,
-  hora_fin TIME NOT NULL,
-  PRIMARY KEY(id_hora)
+CREATE TABLE inscripcion (
+  grupos_id_grupo INTEGER UNSIGNED NOT NULL,
+  asistentes_ci_asis INTEGER UNSIGNED NOT NULL,
+  asistido CHAR(1) NOT NULL,
+  PRIMARY KEY(grupos_id_grupo, asistentes_ci_asis),
+  INDEX asisten_grupo_FKIndex1(asistentes_ci_asis),
+  INDEX asisten_grupo_FKIndex2(grupos_id_grupo)
 );
 
 CREATE TABLE temas (
   id_tema INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  expositores_id_expo INTEGER UNSIGNED NOT NULL,
   nombre_tema VARCHAR(20) NOT NULL,
   descrip_tema VARCHAR(50) NOT NULL,
-  PRIMARY KEY(id_tema)
+  PRIMARY KEY(id_tema),
+  INDEX temas_FKIndex1(expositores_id_expo)
 );
 
 CREATE TABLE usuarios (
