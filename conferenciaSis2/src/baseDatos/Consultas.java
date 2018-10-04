1f4d3a51bd2f66f496b0244bdf1ccd09208e1cda
sabo <sabo@DESKTOP-HNQ9OF4>
2018-10-04 00:31:54 -0400

    Revert "consulta para cancelar inscripcion"
    This reverts commit 643941680c8253db55bd3c4504b403331bb34d63.

--- conferenciaSis2/src/baseDatos/Consultas.java
+++ conferenciaSis2/src/baseDatos/Consultas.java
@@ -21,65 +21,15 @@ public class Consultas {
     Statement sentencia;
     public Consultas(Connection connect){
         conect=connect;
-        try {
-            sentencia=(com.mysql.jdbc.Statement) conect.createStatement();
-        } catch (SQLException ex) {
-            Logger.getLogger(Consultas.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
     public ResultSet query(){
         ResultSet res;
-        
         try {
+            sentencia=(com.mysql.jdbc.Statement) conect.createStatement();
             res= sentencia.executeQuery("SELECT DISTINCT id_grupo  FROM grupos");
         } catch (SQLException ex) {
             Logger.getLogger(Consultas.class.getName()).log(Level.SEVERE, null, ex);
             res=null;
-        }
-        
-        
-        return res;
-    }
-    public boolean validarAsistente(int ciAsistente){
-        boolean res=false;
-        String query= "SELECT * FROM asistentes WHERE ci_asis='"+ciAsistente+"'";
-        ResultSet r;
-        
-        
-        try {
-            
-            r = sentencia.executeQuery(query);
-            if(r.next()){
-                res=true;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(Consultas.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return res;
-        
-    }
-    public boolean cancelarInscripcion(int ciAsistente,int idGrupo){
-        boolean res=false;
-        
-        try {
-            String query="SELECT cupos_dispo FROM grupos WHERE id_grupo='"+idGrupo+"'";
-            ResultSet aux=sentencia.executeQuery(query);
-            int cupos=-1;
-            while(aux.next()){
-                cupos=aux.getInt(1);
-                System.out.println(cupos);
-            }
-            
-            query="DELETE FROM asistentes WHERE ci_asis='"+ciAsistente+"'";
-            sentencia.executeUpdate(query);
-            cupos+=1;
-            query="UPDATE grupos SET cupos_dispo='"+cupos+"' WHERE id_grupo='"+idGrupo+"'";
-            sentencia.executeUpdate(query);
-            res=true;
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(Consultas.class.getName()).log(Level.SEVERE, null, ex);
-            
         }
         return res;
     }
